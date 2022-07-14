57030b8b1d98916b49d19adb149804fcaa8f5156
azarashi <azarashi@majestaria.fun>
2022-07-13 21:09:14 -0400

    Add more asset list functions

--- assets.go
+++ assets.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"os"
+	"path/filepath"
 	"strconv"
 )
 
@@ -24,4 +25,46 @@ func getMapList() []uint16 {
 	}
 
 	return maps
+}
+
+func getCharSetList() []string {
+	files, err := os.ReadDir("CharSet")
+	if err != nil {
+		panic(err)
+	}
+
+	var charsets []string
+	for _, file := range files {
+		charsets = append(charsets, file.Name()[:len(file.Name())-len(filepath.Ext(file.Name()))])
+	}
+
+	return charsets
+}
+
+func getSoundList() []string {
+	files, err := os.ReadDir("Sound")
+	if err != nil {
+		panic(err)
+	}
+
+	var sounds []string
+	for _, file := range files {
+		sounds = append(sounds, file.Name()[:len(file.Name())-len(filepath.Ext(file.Name()))])
+	}
+
+	return sounds
+}
+
+func getSystemList() []string {
+	files, err := os.ReadDir("System")
+	if err != nil {
+		panic(err)
+	}
+
+	var systems []string
+	for _, file := range files {
+		systems = append(systems, file.Name()[:len(file.Name())-len(filepath.Ext(file.Name()))])
+	}
+
+	return systems
 }
