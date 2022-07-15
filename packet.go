9e389c51ce3206c724f170925be5629fd794e4a0
azarashi <azarashi@majestaria.fun>
2022-07-14 20:38:40 -0400

    Optimize switch room order, get client new id

--- packet.go
+++ packet.go
@@ -41,8 +41,9 @@ func (p *Packet) handleSwitchRoom(switchRoom protocol.SwitchRoom) error {
 
 	p.sender.handleDisconnect() // remove from old room and broadcast disconnect packet
 	p.sender.room = p.sender.room.server.rooms[switchRoom.Id] // set client room to new room
-	p.sender.handleConnect() // get room data and broadcast connect packet
+	p.sender.id = p.sender.room.getFreeId() // set client id to new room's free id
 	p.sender.room.server.rooms[switchRoom.Id].clients[p.sender.id] = p.sender // add to new room
+	p.sender.handleConnect() // get room data and broadcast connect packet
 
 	return nil
 }
