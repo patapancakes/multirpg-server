37f746f67eaaa783a73cb6725ce58ec1456ffef5
azarashi <azarashi@majestaria.fun>
2022-07-15 14:08:54 -0400

    Add license header to start of source files

--- protocol/definitions.go
+++ protocol/definitions.go
@@ -1,5 +1,25 @@
 package protocol
 
+/*
+multirpg-server
+https://github.com/Gamizard/multirpg-server
+
+Copyright (C) 2022 azarashi <azarashi@majestaria.fun>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published
+by the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program. If not, see <https://www.gnu.org/licenses/>.
+*/
+
 const (
 	CONNECT     uint8 = 0x01
 	DISCONNECT  uint8 = 0x02
